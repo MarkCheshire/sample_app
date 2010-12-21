0cfd08397ee054ec8223ee55c2437123bacab827
Mark Cheshire <Mark_Cheshire@alum.mit.edu>
2010-12-22 00:17:03 +0100

    Done with sign in

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,9 +27,49 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Sign up")
   end
    
+   
   it "should have an users page at '/users/1'" do
+    @attr = { :name => "Example User", 
+               :email => "user@example.com",
+               :password => "foobar",
+               :password_confirmation => "foobar"
+              }
+    @user = User.create!(@attr)
     get '/users/1'
     response.should be_success
   end
+  
+  describe "when not signed in" do
+    it "should have a signin link" do
+      visit root_path
+      response.should have_selector("a", :href => signin_path,
+                                         :content => "Sign in")
+    end
+  end
+
+  describe "when signed in" do
+
+    before(:each) do
+      @user = Factory(:user)
+      visit signin_path
+      fill_in :email,    :with => @user.email
+      fill_in :password, :with => @user.password
+      click_button
+    end
+
+    it "should have a signout link" do
+      visit root_path
+      response.should have_selector("a", :href => signout_path,
+                                         :content => "Sign out")
+    end
+
+    it "should have a profile link" do
+      visit root_path
+      response.should have_selector("a", :href => user_path(@user),
+                                         :content => "Profile")
+    end
+
+  end
+  
    
 end
