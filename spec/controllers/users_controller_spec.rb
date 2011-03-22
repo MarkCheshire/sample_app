f46e4a09f423d88158bb1d41652c9a13afb61717
Mark Cheshire <Mark_Cheshire@alum.mit.edu>
2011-03-22 01:04:18 +0100

    Upto sample microposts 11.2.2

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -34,6 +34,14 @@ describe UsersController do
       response.should have_selector("h1>img", :class => "gravatar")
     end
 
+    it "should show the user's microposts" do
+      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+      mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+      get :show, :id => @user
+      response.should have_selector("span.content", :content => mp1.content)
+      response.should have_selector("span.content", :content => mp2.content)
+    end
+
   end
 
   describe "GET 'new'" do
@@ -166,7 +174,7 @@ describe UsersController do
       it "should change the user's attributes" do
         put :update, :id => @user, :user => @attr
         @user.reload
-        @user.name.should  == @attr[:name]
+        @user.name.should == @attr[:name]
         @user.email.should == @attr[:email]
       end
 
